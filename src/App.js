962f01609593354bb9720eb21e156acdb0bb8048
YiGunHa <isinegajok@naver.com>
2025-01-08 01:01:12 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { useEffect, useState } from 'react';
+
+function App() {
+  const [message, setMessage] = useState('');
+
+  useEffect(() => {
+    // 백엔드 API 호출
+    fetch('http://localhost:5000')
+      .then(response => response.json())
+      .then(data => setMessage(data.message))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
+  return (
+    <div className="App">
+      <h1>{message}</h1>
+    </div>
+  );
+}
+
+export default App;
