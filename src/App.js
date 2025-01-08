7a180e35ae5ae709ed57f4bf1e6813dfc722a217
YiGunHa <isinegajok@naver.com>
2025-01-08 22:52:13 +0800

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
     e.preventDefault();
 
     try {
-      const response = await fetch('https://linkback-production.up.railway.app/login', {
+      const response = await fetch('https://linkdoor-production.up.railway.app/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
